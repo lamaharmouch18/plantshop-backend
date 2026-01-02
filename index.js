f9f5f527e9fe026f03ec1b16ada5e1a335d70e06
lamaharmouch18 <52330808@students.liu.edu.lb>
2026-01-02 23:44:33 +0200

    Use Aiven DB with env variables and SSL

--- index.js
+++ index.js
@@ -12,17 +12,14 @@ app.use(bodyParser.json());
    MySQL CONNECTION POOL (FIXED)
    =============================== */
   
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
